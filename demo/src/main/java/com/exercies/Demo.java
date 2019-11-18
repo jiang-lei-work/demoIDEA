722e5ef4c0521c1ac3235b6bdd16b04cd0da11b0
Jiang <jiang@163.com>
2019-11-18 21:30:02 +0800

    更改demo工程

--- demo/src/main/java/com/exercies/Demo.java
+++ demo/src/main/java/com/exercies/Demo.java
@@ -3,5 +3,12 @@ package com.exercies;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("git练习");
+        Demo demo = new Demo();
+        demo.text();
+    }
+    public void text(){
+        int i = 0;
+        i+=10;
+        System.out.println(i);
     }
 }
